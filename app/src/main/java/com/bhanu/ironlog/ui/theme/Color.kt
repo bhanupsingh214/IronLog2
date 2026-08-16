1b7c197ff6b007d6ffd0049ba6b8d58aebab32cc
bhanupsingh214 <bhanujudo@gmail.com>
2026-08-17 00:20:56 +0530

    feat: establish IronLog Phase 6 color tokens

--- app/src/main/java/com/bhanu/ironlog/ui/theme/Color.kt
+++ app/src/main/java/com/bhanu/ironlog/ui/theme/Color.kt
@@ -2,10 +2,27 @@ package com.bhanu.ironlog.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Phase 6 Figma foundation tokens.
+val IronLogSurface0 = Color(0xFF050811)
+val IronLogSurface1 = Color(0xFF0B1020)
+val IronLogSurface2 = Color(0xFF121A2A)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val IronLogPrimary = Color(0xFF6C3DFF)
+val IronLogPrimaryLight = Color(0xFFA78BFA)
+val IronLogAccent = Color(0xFF00D4FF)
+
+val IronLogSuccess = Color(0xFF22C55E)
+val IronLogWarning = Color(0xFFF59E0B)
+val IronLogDanger = Color(0xFFEF4444)
+
+val IronLogTextPrimary = Color(0xFFF7F7FB)
+val IronLogTextSecondary = Color(0xFFB8C0D1)
+val IronLogTextMuted = Color(0xFF7F8AA3)
+
+// Compatibility aliases for any existing screens that still reference the starter theme names.
+val Purple80 = IronLogPrimaryLight
+val PurpleGrey80 = IronLogTextSecondary
+val Pink80 = IronLogAccent
+val Purple40 = IronLogPrimary
+val PurpleGrey40 = IronLogTextSecondary
+val Pink40 = IronLogAccent
