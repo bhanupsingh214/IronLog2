3aae28cbf3adec64572acb92d94afa6c23955848
bhanupsingh214 <bhanujudo@gmail.com>
2026-08-17 00:22:05 +0530

    fix: add Material Scaffold import

--- app/src/main/java/com/bhanu/ironlog/ui/screens/MainScreen.kt
+++ app/src/main/java/com/bhanu/ironlog/ui/screens/MainScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
